b6c048a1744c64e4ab28469c5adc01e6df837fbb
eunie9498 <bvegemilb@naver.com>
2022-05-31 22:30:41 +0900

    add Model

--- app/src/main/java/com/bookting/models/MainModels.kt
+++ app/src/main/java/com/bookting/models/MainModels.kt
@@ -9,3 +9,22 @@ data class Users(
     val pw: String? = ""
 ) : Parcelable
 
+@Parcelize
+data class GetBookDetailResponse(
+    val result: String? = "",
+    val reason: String? = "",
+    val data: BookItem
+): Parcelable
+
+@Parcelize
+data class BookItem(
+    val author: String,
+    val detail_info: String,
+    val id: Int,
+    val image_url: String,
+    val isbn: String,
+    val name: String,
+    val price: Int,
+    val publication_date: String,
+    val publisher: String
+) : Parcelable
