3f3efc1b30f741af694e3bfb39771c72ab57febf
eunie9498 <bvegemilb@naver.com>
2022-06-11 23:37:37 +0900

    [API 추가] 회원가입

--- app/src/main/java/com/bookting/api/BookAPI.kt
+++ app/src/main/java/com/bookting/api/BookAPI.kt
@@ -1,10 +1,17 @@
 package com.bookting.api
 
-import com.bookting.models.GetBookDetailResponse
+import com.bookting.data.GetBookDetailResponse
+import com.bookting.data.JoinBody
+import com.bookting.data.ResultResponse
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface BookAPI {
     @GET("books/{bookId}")
     fun getBooks(): Call<GetBookDetailResponse>
+
+    @POST("users")
+    fun joinUser(@Body body: JoinBody): Call<ResultResponse>
 }
