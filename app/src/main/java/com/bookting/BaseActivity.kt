d368b66342ee1b442c9f9b35d726a5c07dbe0b1a
eunie9498 <bvegemilb@naver.com>
2022-06-12 01:11:48 +0900

    BaseActivity 수정

---
+++ app/src/main/java/com/bookting/BaseActivity.kt
@@ -0,0 +1,21 @@
+package com.bookting
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+open class BaseActivity<T : ViewDataBinding>(@LayoutRes val layoutRes: Int) : AppCompatActivity() {
+
+    lateinit var binding: T
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutRes)
+        binding.onCreate()
+    }
+
+    open fun T.onCreate() = Unit
+
+}
