f36d2da1eae10cfa0ea19b62a781758856b50643
eunie9498 <bvegemilb@naver.com>
2022-06-16 22:42:28 +0900

    [JoinActivity] with Databinding

--- app/src/main/java/com/bookting/view/start/JoinActivity.kt
+++ app/src/main/java/com/bookting/view/start/JoinActivity.kt
@@ -1,20 +1,38 @@
 package com.bookting.view.start
 
+import android.widget.Toast
 import com.bookting.BaseActivity
 import com.bookting.R
 import com.bookting.databinding.ActivityJoinBinding
+import com.bookting.utils.setStatusTrans
 
 class JoinActivity : BaseActivity<ActivityJoinBinding>(R.layout.activity_join) {
 
     override fun ActivityJoinBinding.onCreate() {
-        idField.setBg(R.drawable.line_radius10_purple400)
-        idField.setHt(root.context.getString(R.string.id))
-
-        pwField.setBg(R.drawable.line_radius10_purple400)
-        pwField.setErr(root.context.getString(R.string.pw))
+        act = this@JoinActivity
+        
+        idField.apply {
+            setHtColor(R.color.purple400)
+            setHt(root.context.getString(R.string.id))
+        }
+        pwField.apply {
+            setHt(root.context.getString(R.string.pw))
+            setHtColor(R.color.purple400)
+        }
 
         appBar.binding.btnBack.setOnClickListener {
             onBackPressed()
         }
+
+        nickField.apply {
+            setHtColor(R.color.purple400)
+            setHt(root.context.getString(R.string.nickname))
+        }
+
+        join()
+    }
+
+     fun join() = with(binding) {
+
     }
 }
