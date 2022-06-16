d2ec206105c9e3d396c2976c35e16a29fccf29f0
eunie9498 <bvegemilb@naver.com>
2022-06-17 01:09:43 +0900

    [FCM] getToken

--- app/src/main/java/com/bookting/main/MainActivity.kt
+++ app/src/main/java/com/bookting/main/MainActivity.kt
@@ -4,17 +4,33 @@ package com.bookting.main
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.bookting.BaseActivity
+import com.bookting.FbMessaging
 import com.bookting.R
+import com.bookting.data.MainConstants
 import com.bookting.databinding.ActivityMainBinding
 import com.bookting.utils.setStatusTrans
 import com.bookting.view.start.JoinActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.FirebaseMessagingService
 
 open class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(
+            OnCompleteListener { task ->
+                if(!task.isSuccessful){
+                    Log.e("FCM Err", task.exception.toString())
+                    return@OnCompleteListener
+                }
+                sharedHelper.addPreference(MainConstants.Shared.TOKEN, task.result)
+            }
+        )
         val intent = Intent(this, JoinActivity::class.java)
         startActivity(intent)
     }
