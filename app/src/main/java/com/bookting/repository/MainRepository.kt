9a2d31834a82cb17a92b275aac7f6cb1a0a996a0
eunie9498 <bvegemilb@naver.com>
2022-06-20 23:18:46 +0900

    Repository 생성

---
+++ app/src/main/java/com/bookting/repository/MainRepository.kt
@@ -0,0 +1,30 @@
+package com.bookting.repository
+
+import com.bookting.api.BookAPI
+import com.bookting.data.JoinBody
+import com.bookting.data.MainConstants
+import com.bookting.data.Users
+import com.bookting.viewmodel.MainViewModel
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import javax.inject.Inject
+
+class MainRepository {
+    @Inject
+    lateinit var api: BookAPI
+    var viewModel: MainViewModel = MainViewModel()
+    
+    fun join(body: JoinBody): Disposable {
+        return api.run {
+            joinUser(body)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    if(it.result == MainConstants.SUCCESS) {
+
+                    }
+                }
+        }
+    }
+}
