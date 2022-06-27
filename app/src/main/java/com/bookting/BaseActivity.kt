d69bd1800be6e30eaed0b2ab6658e73fc6f8affc
eunie9498 <bvegemilb@naver.com>
2022-06-27 23:34:55 +0900

    :sparkles: Toast, moveToMain

--- app/src/main/java/com/bookting/BaseActivity.kt
+++ app/src/main/java/com/bookting/BaseActivity.kt
@@ -1,12 +1,18 @@
 package com.bookting
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.bookting.repository.MainRepository
+import com.bookting.ui.ToastOneBtn
+import com.bookting.view.main.MainActivity
 import com.bookting.view.main.NetworkActivity
 import com.bookting.viewmodel.MainViewModel
 
@@ -18,7 +24,7 @@ open class BaseActivity<T : ViewDataBinding>(@LayoutRes val layoutRes: Int) : Ne
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_start)
 
         binding = DataBindingUtil.setContentView(this, layoutRes)
         val factory = ViewModelFactory(MainRepository(this))
@@ -35,5 +41,27 @@ open class BaseActivity<T : ViewDataBinding>(@LayoutRes val layoutRes: Int) : Ne
     }
 
     open fun T.onCreate() = Unit
+
+    fun showToast(toast: String){
+        Toast.makeText(binding.root.context, toast, Toast.LENGTH_SHORT).show()
+    }
+
+    fun showToastOneBtn(title: String, msg: String, btnTxt: String, btnListener : View.OnClickListener){
+        val onebtn = ToastOneBtn(this@BaseActivity)
+        onebtn.apply {
+            setTitle(title)
+            setMsg(msg)
+            setBtn(btnTxt, {
+                btnListener
+                dismiss()
+            })
+        }
+        onebtn.show()
+    }
+
+    fun moveToMain(){
+        val i = Intent(this, MainActivity::class.java)
+        startActivity(i)
+    }
 
 }
