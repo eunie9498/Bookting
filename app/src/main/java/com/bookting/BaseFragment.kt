44be332b2932c65eb2d2e15ea8489ee174afe394
eunie9498 <bvegemilb@naver.com>
2022-07-11 21:43:14 +0900

    :zap: BaseFragment with ViewModel

--- app/src/main/java/com/bookting/BaseFragment.kt
+++ app/src/main/java/com/bookting/BaseFragment.kt
@@ -8,14 +8,18 @@ import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.bookting.data.SharedHelper
 import com.bookting.repository.MainRepository
+import com.bookting.viewmodel.MainViewModel
 
 open class BaseFragment<T : ViewDataBinding>(@LayoutRes val layoutRes: Int) : Fragment() {
 
     lateinit var binding: T
     lateinit var repository: MainRepository
     lateinit var sharedHelper: SharedHelper
+    lateinit var viewModel: MainViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,7 +34,17 @@ open class BaseFragment<T : ViewDataBinding>(@LayoutRes val layoutRes: Int) : Fr
         super.onViewCreated(view, savedInstanceState)
         repository = MainRepository(requireContext())
         sharedHelper = SharedHelper(requireContext())
+
+        val factory = ViewModelFactory(MainRepository(requireContext()))
+        viewModel = ViewModelProvider(requireActivity(), factory).get(MainViewModel::class.java)
+
         binding.initView()
+    }
+
+    inner class ViewModelFactory(val repository: MainRepository) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return modelClass.getConstructor(MainRepository::class.java).newInstance(repository)
+        }
     }
 
     open fun T.initView() = Unit
