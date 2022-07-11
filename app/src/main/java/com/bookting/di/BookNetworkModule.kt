408be71142d8a259e4746e20d912771045b1408d
eunie9498 <bvegemilb@naver.com>
2022-07-11 21:24:28 +0900

    :heavy_plus_sign: Retrofit add Interceptor

--- app/src/main/java/com/bookting/di/BookNetworkModule.kt
+++ app/src/main/java/com/bookting/di/BookNetworkModule.kt
@@ -9,10 +9,13 @@ import com.bookting.data.MainConstants
 import com.bookting.data.SharedHelper
 import dagger.Module
 import dagger.Provides
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
 import javax.inject.Singleton
 
 @Module
@@ -23,18 +26,30 @@ class BookNetworkModule(val context: Context) {
 
     @Provides
     @Singleton
-    fun provideAPI(): BookAPI {
-        return Retrofit.Builder().baseUrl(MainConstants.BASE_URL)
-            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(BookAPI::class.java)
+    fun provideSharedPreferences(): SharedHelper{
+        return SharedHelper(context)
     }
 
     @Provides
     @Singleton
-    fun provideSharedPreferences(): SharedHelper{
-        return SharedHelper(context)
+    fun provideAPI(): BookAPI {
+        return Retrofit.Builder().baseUrl(MainConstants.BASE_URL)
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(OkHttpClient.Builder().addInterceptor(AppInterceptor(provideSharedPreferences())).build())
+            .build()
+            .create(BookAPI::class.java)
+    }
+    class AppInterceptor(val sharedHelper: SharedHelper) : Interceptor {
+        @Throws(IOException::class)
+        override fun intercept(chain: Interceptor.Chain)
+                : Response = with(chain) {
+            val newRequest = request().newBuilder()
+                .addHeader("Authorization", "Bearer " + sharedHelper.getRefreshToken)
+                .build()
+
+            proceed(newRequest)
+        }
     }
 
 }
