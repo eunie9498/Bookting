447ef979d9b8c88e366199ab81e4e44756c34573
eunie9498 <bvegemilb@naver.com>
2022-07-23 23:15:08 +0900

    :lipstick: ImageView Util

--- app/src/main/java/com/bookting/utils/BooktingUtils.kt
+++ app/src/main/java/com/bookting/utils/BooktingUtils.kt
@@ -6,13 +6,12 @@ import android.graphics.Color
 import android.os.Build
 import android.util.Base64
 import android.util.TypedValue
-import android.view.View
-import android.view.WindowInsets
-import android.view.WindowInsetsController
-import android.view.WindowManager
+import android.view.*
 import android.widget.ImageView
 import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import java.nio.charset.Charset
 import javax.crypto.Cipher
 import javax.crypto.spec.IvParameterSpec
@@ -64,4 +63,8 @@ fun Activity.setFullScreen() {
 
 fun ImageView.setImg(uri: Any) {
     Glide.with(this).load(uri).into(this)
+}
+
+fun ImageView.setRoundImg(uri: Any) {
+    Glide.with(this).load(uri).apply(RequestOptions.bitmapTransform(RoundedCorners(10))).into(this)
 }
