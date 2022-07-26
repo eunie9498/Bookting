326fc734f732bf9eed6a5362e6b1d2b45be952f2
eunie9498 <bvegemilb@naver.com>
2022-07-26 23:52:25 +0900

    :zap: Login Error with Expire Token

--- app/src/main/java/com/bookting/view/start/LoginActivity.kt
+++ app/src/main/java/com/bookting/view/start/LoginActivity.kt
@@ -1,7 +1,5 @@
 package com.bookting.view.start
 
-import android.util.Log
-import androidx.core.view.get
 import androidx.lifecycle.Observer
 import com.bookting.BaseActivity
 import com.bookting.R
@@ -9,13 +7,8 @@ import com.bookting.data.LoginBody
 import com.bookting.data.MainConstants
 import com.bookting.databinding.ActivityLoginBinding
 import com.bookting.utils.setFullScreen
-import com.bookting.utils.setImg
-import com.bookting.utils.setStatusTrans
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.schedulers.Schedulers
 
 class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
-
 
     override fun ActivityLoginBinding.onCreate() {
 
@@ -28,17 +21,40 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login
 
     }
 
-    fun login()
-    {
-        viewModel.login(LoginBody(
-            email = binding.idField.getEt(),
-            sharedHelper.newEncrypt(binding.pwField.getEt().toByteArray()),
-            sharedHelper.getFbToken.substring(0, 32)
-        ))
+    fun login() {
+        viewModel.login(
+            LoginBody(
+                email = binding.idField.getEt(),
+                sharedHelper.newEncrypt(binding.pwField.getEt().toByteArray()),
+                sharedHelper.getFbToken.substring(0, 32)
+            )
+        )
 
         viewModel.loginResponse.observe(this, Observer {
-            if(it.result == MainConstants.SUCCESS) {
+            if (it.result == MainConstants.SUCCESS) {
                 moveToMain()
+                repository.sharedHelper.addPreference(
+                    MainConstants.Shared.REFRESH_TOKEN,
+                    it.data!!.refresh_token
+                )
+                repository.sharedHelper.addPreference(
+                    MainConstants.Shared.ACCESS_TOKEN,
+                    it.data!!.access_token
+                )
+            } else {
+                if ((it.reason ?: "").contains("token")) {
+                    showBtnOneDlg(
+                        getString(R.string.login_err_title),
+                        getString(R.string.login_err_msg_expire),
+                        getString(R.string.ok)
+                    )
+                } else {
+                    showBtnOneDlg(
+                        getString(R.string.login_err_title),
+                        getString(R.string.login_err_msg),
+                        getString(R.string.ok)
+                    )
+                }
             }
         })
 
