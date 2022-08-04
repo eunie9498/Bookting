55ab503085c6fd3fdac3cb8398f022a3f962175d
eunie9498 <bvegemilb@naver.com>
2022-08-05 00:07:21 +0900

    :sparkles: MainActivity Backbutton with auto exit

--- app/src/main/java/com/bookting/view/main/MainActivity.kt
+++ app/src/main/java/com/bookting/view/main/MainActivity.kt
@@ -1,18 +1,23 @@
 package com.bookting.view.main
 
+import android.content.Intent
 import com.bookting.BaseActivity
 import com.bookting.R
 import com.bookting.databinding.ActivityMainBinding
 import com.bookting.ui.TabItem
 import com.bookting.view.main.home.HomeFragment
 
+
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     lateinit var tabs: Array<TabItem>
+    var backPressTime = 0L
 
     override fun ActivityMainBinding.onCreate() {
         binding.activity = this@MainActivity
         tabs = arrayOf(binding.tabHome, binding.tabList, binding.tabSearch, binding.tabSetting)
-        tabClick(binding.tabHome)
+
+        val fragment = HomeFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.frame, fragment).commit()
     }
 
     fun tabClick(v: TabItem) = with(binding) {
@@ -37,6 +42,21 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
             else -> {
 
             }
+        }
+    }
+
+    override fun onBackPressed() {
+
+        if (System.currentTimeMillis() > backPressTime + 3000) {
+            backPressTime = System.currentTimeMillis()
+            showToast(getString(R.string.back_press_exit))
+            return
+        }
+
+        if (System.currentTimeMillis() <= backPressTime + 3000) {
+            moveTaskToBack(true)
+            finishAndRemoveTask()
+            android.os.Process.killProcess(android.os.Process.myPid())
         }
     }
 }
