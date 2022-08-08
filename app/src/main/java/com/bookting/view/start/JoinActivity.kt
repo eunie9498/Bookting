99d969cbf6656158a2b718af5fc9146e31ccca4e
eunie9498 <bvegemilb@naver.com>
2022-08-08 23:08:52 +0900

    :sparkles: 회원가입 시 자동 로그인 후 메인화면 이동

--- app/src/main/java/com/bookting/view/start/JoinActivity.kt
+++ app/src/main/java/com/bookting/view/start/JoinActivity.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import com.bookting.BaseActivity
 import com.bookting.R
 import com.bookting.data.JoinBody
+import com.bookting.data.LoginBody
 import com.bookting.data.MainConstants
 import com.bookting.databinding.ActivityJoinBinding
 import com.google.gson.Gson
@@ -41,6 +42,37 @@ class JoinActivity : BaseActivity<ActivityJoinBinding>(R.layout.activity_join) {
             password = sharedHelper.newEncrypt(pwField.getEt().toByteArray()),
             secret_key = sharedHelper.getFbToken.substring(0, 32), sex
         )
-        viewModel.join(body)
+        userViewModel.join(body)
+        userViewModel.joinResponse.observe(this@JoinActivity) {
+            if (it.result == MainConstants.SUCCESS) {
+                userViewModel.login(
+                    LoginBody(
+                        email = idField.getEt(),
+                        sharedHelper.newEncrypt(pwField.getEt().toByteArray()),
+                        sharedHelper.getFbToken.substring(0, 32)
+                    )
+                )
+
+                userViewModel.loginResponse.observe(this@JoinActivity) {
+                    if (it.result == MainConstants.SUCCESS) {
+                        moveToMain()
+                        repository.sharedHelper.addPreference(
+                            MainConstants.Shared.REFRESH_TOKEN,
+                            it.data!!.refresh_token
+                        )
+                        repository.sharedHelper.addPreference(
+                            MainConstants.Shared.ACCESS_TOKEN,
+                            it.data!!.access_token
+                        )
+                    }
+                }
+            } else {
+                showBtnOneDlg(
+                    getString(R.string.join_err_title),
+                    getString(R.string.join_err_msg),
+                    getString(R.string.ok)
+                )
+            }
+        }
     }
 }
