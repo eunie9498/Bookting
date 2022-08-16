fe8ca07a0c5d0bc34cbba998e8b3cbeac3a0a2c9
eunie9498 <bvegemilb@naver.com>
2022-08-16 23:28:46 +0900

    :recycle: SplashActivity 추가

---
+++ app/src/main/java/com/bookting/SplashActivity.kt
@@ -0,0 +1,53 @@
+package com.bookting
+
+import android.content.Intent
+import com.bookting.data.MainConstants
+import com.bookting.databinding.ActivitySplashBinding
+import com.bookting.view.main.MainActivity
+import com.bookting.view.main.StartActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
+
+class SplashActivity: BaseActivity<ActivitySplashBinding>(R.layout.activity_splash) {
+
+    override fun ActivitySplashBinding.onCreate() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(
+            OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    return@OnCompleteListener
+                }
+                sharedHelper.addPreference(MainConstants.Shared.FB_TOKEN, task.result)
+            }
+        )
+
+        if (sharedHelper.getAccessToken != "") {
+            userViewModel.getUserData()
+            userViewModel.userDataResponse.observe(this@SplashActivity) {
+                if (it.result == MainConstants.SUCCESS) {
+                    it.data?.let { data ->
+                        sharedHelper.addPreference(MainConstants.Shared.USER_EMAIL, data.email)
+                        sharedHelper.addPreference(MainConstants.Shared.USER_NICK, data.nickname)
+                        sharedHelper.addPreference(MainConstants.Shared.USER_PROFILE, data.image_url)
+                    }
+                    val intent = Intent(this@SplashActivity, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    if (it.reason!!.contains("access_token")) {
+                        showBtnOneDlg(
+                            getString(R.string.login_err_title),
+                            getString(R.string.login_err_msg_expire), getString(R.string.ok)
+                        ) {
+                            sharedHelper.addPreference(MainConstants.Shared.ACCESS_TOKEN, "")
+                            sharedHelper.addPreference(MainConstants.Shared.REFRESH_TOKEN, "")
+
+                            val i = Intent(this@SplashActivity, StartActivity::class.java)
+                            i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+                            startActivity(i)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
--- app/src/main/java/com/bookting/view/main/StartActivity.kt
+++ app/src/main/java/com/bookting/view/main/StartActivity.kt
@@ -2,60 +2,15 @@ package com.bookting.view.main
 
 
 import android.content.Intent
-import android.os.Bundle
-import android.util.Log
 import com.bookting.BaseActivity
 import com.bookting.R
-import com.bookting.data.MainConstants
 import com.bookting.databinding.ActivityStartBinding
 import com.bookting.view.start.JoinActivity
 import com.bookting.view.start.LoginActivity
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.messaging.FirebaseMessaging
-import com.google.gson.Gson
-
 open class StartActivity : BaseActivity<ActivityStartBinding>(R.layout.activity_start) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        FirebaseMessaging.getInstance().token.addOnCompleteListener(
-            OnCompleteListener { task ->
-                if (!task.isSuccessful) {
-                    return@OnCompleteListener
-                }
-                sharedHelper.addPreference(MainConstants.Shared.FB_TOKEN, task.result)
-            }
-        )
-        binding.activity = this
-
-        if (sharedHelper.getAccessToken != "") {
-            userViewModel.getUserData()
-            userViewModel.userDataResponse.observe(this@StartActivity) {
-                if (it.result == MainConstants.SUCCESS) {
-                    it.data?.let { data ->
-                        sharedHelper.addPreference(MainConstants.Shared.USER_EMAIL, data.email)
-                        sharedHelper.addPreference(MainConstants.Shared.USER_NICK, data.nickname)
-                        sharedHelper.addPreference(MainConstants.Shared.USER_PROFILE, data.image_url)
-                    }
-                    goMain()
-                } else {
-                    if (it.reason!!.contains("access_token")) {
-                        showBtnOneDlg(
-                            getString(R.string.login_err_title),
-                            getString(R.string.login_err_msg_expire), getString(R.string.ok)
-                        ) {
-                            sharedHelper.addPreference(MainConstants.Shared.ACCESS_TOKEN, "")
-                            sharedHelper.addPreference(MainConstants.Shared.REFRESH_TOKEN, "")
-
-                            val i = Intent(this, StartActivity::class.java)
-                            i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
-                            startActivity(i)
-                        }
-                    }
-                }
-            }
-        }
+    override fun ActivityStartBinding.onCreate() {
+        binding.activity = this@StartActivity
     }
 
     fun goJoin() {
@@ -65,11 +20,6 @@ open class StartActivity : BaseActivity<ActivityStartBinding>(R.layout.activity_
 
     fun goLogin() {
         val intent = Intent(this, LoginActivity::class.java)
-        startActivity(intent)
-    }
-
-    fun goMain() {
-        val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
     }
 }
