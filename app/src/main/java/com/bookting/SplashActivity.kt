46ac7bf17179e9345b09e1f78dd4fb9a72083476
eunie9498 <bvegemilb@naver.com>
2022-08-23 17:13:58 +0900

    :zap: User 정보 API 호출 , Sharedpreferences 저장

--- app/src/main/java/com/bookting/SplashActivity.kt
+++ app/src/main/java/com/bookting/SplashActivity.kt
@@ -7,8 +7,10 @@ import com.bookting.view.main.MainActivity
 import com.bookting.view.main.StartActivity
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.messaging.FirebaseMessaging
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.schedulers.Schedulers
 
-class SplashActivity: BaseActivity<ActivitySplashBinding>(R.layout.activity_splash) {
+class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_splash) {
 
     override fun ActivitySplashBinding.onCreate() {
         FirebaseMessaging.getInstance().token.addOnCompleteListener(
@@ -21,33 +23,44 @@ class SplashActivity: BaseActivity<ActivitySplashBinding>(R.layout.activity_spla
         )
 
         if (sharedHelper.getAccessToken != "") {
-            userViewModel.getUserData()
-            userViewModel.userDataResponse.observe(this@SplashActivity) {
-                if (it.result == MainConstants.SUCCESS) {
-                    it.data?.let { data ->
-                        sharedHelper.addPreference(MainConstants.Shared.USER_EMAIL, data.email)
-                        sharedHelper.addPreference(MainConstants.Shared.USER_NICK, data.nickname)
-                        sharedHelper.addPreference(MainConstants.Shared.USER_PROFILE, data.image_url)
-                    }
-                    val intent = Intent(this@SplashActivity, MainActivity::class.java)
-                    startActivity(intent)
-                } else {
-                    if (it.reason!!.contains("access_token")) {
-                        sharedHelper.addPreference(MainConstants.Shared.ACCESS_TOKEN, "")
-                        sharedHelper.addPreference(MainConstants.Shared.REFRESH_TOKEN, "")
+            val map = mutableMapOf<String, String>()
+            map.put("access_token", sharedHelper.getAccessToken)
+            api.getUserData(map).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    if (it.result == MainConstants.SUCCESS) {
+                        it.data?.let { data ->
+                            sharedHelper.addPreference(MainConstants.Shared.USER_EMAIL, data.email)
+                            sharedHelper.addPreference(
+                                MainConstants.Shared.USER_NICK,
+                                data.nickname
+                            )
+                            sharedHelper.addPreference(
+                                MainConstants.Shared.USER_PROFILE,
+                                data.image_url
+                            )
+                        }
+                        val intent = Intent(this@SplashActivity, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        if (it.reason!!.contains("access_token")) {
+                            sharedHelper.addPreference(MainConstants.Shared.ACCESS_TOKEN, "")
+                            sharedHelper.addPreference(MainConstants.Shared.REFRESH_TOKEN, "")
 
-                        showBtnOneDlg(
-                            getString(R.string.login_err_title),
-                            getString(R.string.login_err_msg_expire), getString(R.string.ok)
-                        ) {
-                            val i = Intent(this@SplashActivity, StartActivity::class.java)
-                            i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
-                            startActivity(i)
+                            showBtnOneDlg(
+                                getString(R.string.login_err_title),
+                                getString(R.string.login_err_msg_expire), getString(R.string.ok)
+                            ) {
+                                val i = Intent(this@SplashActivity, StartActivity::class.java)
+                                i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+                                startActivity(i)
+                            }
                         }
                     }
-                }
-            }
-        }else{
+                }, {
+
+                })
+        } else {
             val i = Intent(this@SplashActivity, StartActivity::class.java)
             startActivity(i)
         }
